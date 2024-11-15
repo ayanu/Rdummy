79354b5ff00dce421b84d05132953a7b72a4897f
Stephan Henne <stephan.henne@empa.ch>
2024-11-15 16:52:22 +0100

    Initil commit

---
+++ R/dummy.R
@@ -0,0 +1,11 @@
+#' Add two numbers
+#'
+#' Function adds to numbers x and y
+#' 
+#' @param x First number
+#' @param y Second number
+#'
+#' @return Sum of x and y
+#' @export
+dummy <-
+function(x,y) { x+y }
