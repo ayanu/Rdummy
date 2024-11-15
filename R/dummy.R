d13ed82e28a19109d04ddfe4ec5d804d9e5e617b
Stephan Henne <stephan.henne@empa.ch>
2024-11-15 20:45:58 +0100

    Update

--- R/dummy.R
+++ R/dummy.R
@@ -5,7 +5,10 @@
 #' @param x First number
 #' @param y Second number
 #'
+#' @author stephan.henne@empa.ch
 #' @return Sum of x and y
 #' @export
 dummy <-
-function(x,y) { x+y }
+function(x,y) { 
+	return(x+y)
+}
